988a2674731aa2fbf14d45cb481e919522c37d31
awadhraj yadav <awadhraj42011@gmail.com>
2020-08-22 11:41:22 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('products.create');
+});
+
+Route::resource('products','ProductController');
+
+// 127.0.0.1:8000/products
+// Route::get('products','ProductController@index');
+
